77f4ea294b7f39f44d39dfff0d3187faec10fb96
hungthinh1602 <hungthinh16022000@gmail.com>
2024-07-03 16:54:07 +0700

    Update Order Status

--- backend/src/routes/orderRoutes.ts
+++ backend/src/routes/orderRoutes.ts
@@ -8,7 +8,9 @@ const orderRoutes = router;
 
 router.use(verifyJWT);
 router.route("/auto-create-order-status").post(orderController.autoCreateStatus);
+router.route("/update-order-status/:orderId").post(orderController.updateOrderStatus)
 router.route("/create-new-order").post(orderController.createOrder);
-router.route("/get-order-status").get(orderController.getOrderStatus);
+router.route("/get-order-status").post(orderController.getOrderStatus);
+
 
 export default orderRoutes;
