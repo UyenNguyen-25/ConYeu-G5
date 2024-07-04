78f77ea9381488bcbdf0a940c480d86091b1d2dc
hungthinh1602 <hungthinh16022000@gmail.com>
2024-07-04 17:55:12 +0700

    payment

--- backend/src/routes/onlineCheckoutRoutes.ts
+++ backend/src/routes/onlineCheckoutRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import onlineCheckoutController from "../controllers/onlineCheckoutController";
+import verifyJWT from "../middleware/verifyJWT";
 
 const router = Router();
 
 const onlineCheckoutRoutes = router;
 
+// router.use(verifyJWT);
 
 router.route("/payment").get(onlineCheckoutController.createPayment);
 router.route("/paymentZalo").get(onlineCheckoutController.createPaymentZaloPay);
